8d9edba1ad5fc419d918879e77eeacd73bd9e74f
asraful-07 <asraful0508@gmail.com>
2026-02-23 08:54:39 +0600

    delete my old code

--- pop.js
+++
@@ -1,6 +0,0 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-console.log(fruits);
-const out1 = fruits.pop();
-console.log(fruits);
-console.log(out1);
--- splice.js
+++
@@ -1,5 +0,0 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-console.log(fruits);
-// fruits.splice(1, 1, "Lemon", "Kiwi");
-console.log(fruits.splice(1, 1, "Lemon", "Kiwi"));
