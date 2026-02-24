591f9db149d6bbf80fc34627371fc18b3e316952
asraful-07 <asraful0508@gmail.com>
2026-02-24 11:59:51 +0600

    setup tanstack-query done

---
+++ src/app/(commonLayout)/consultation/doctor/[id]/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ConsultationDoctorDynamicPage() {
+  return <div>Consultation Doctor Dynamic Page</div>;
+}
--- src/app/(commonLayout)/consultation/page.tsx
+++ src/app/(commonLayout)/consultation/page.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import DoctorsList from "@/components/modules/Consultation/DoctorsList";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import { getDoctors } from "./_actions";
 
-export default function page() {
-  return <div>page</div>;
-}
+const ConsultationPage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["doctor"],
+    queryFn: getDoctors,
+  });
+  return (
+    // Neat! Serialization is now as easy as passing props.
+    // HydrationBoundary is a Client Component, so hydration will happen there.
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DoctorsList />
+    </HydrationBoundary>
+  );
+};
+
+export default ConsultationPage;
